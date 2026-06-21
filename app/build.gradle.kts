43c23637deeb7051890dca59c7e2569104203036
tyer255 <mridulnareda56@gmail.com>
2026-06-21 16:33:45 +0530

    fix: remove unnecessary lint-gradle dependency causing build failure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,4 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.android.tools.lint:lint-gradle:31.1.0")
 }
