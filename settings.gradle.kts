34a2407154c246294a9c9e9cfdb69fc917a01bd3
tyer255 <mridulnareda56@gmail.com>
2026-06-21 15:51:43 +0530

    Migrate from YAML to Kotlin DSL for Gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,16 @@
-name: Build Jarvis APK
-
-on:
-  push:
-    branches: [ "main" ]
-
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-    - uses: actions/checkout@v3
-    - name: Set up JDK
-      uses: actions/setup-java@v3
-      with:
-        java-version: '17'
-        distribution: 'temurin'
-    - name: Setup Gradle
-      uses: gradle/gradle-build-action@v2
-    - name: Build APK
-      run: ./gradlew assembleRelease
-    - name: Upload APK
-      uses: actions/upload-artifact@v3
-      with:
-        name: jarvis-app
-        path: app/build/outputs/apk/release/app-release-unsigned.apk
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Jarvis"
+include(":app")
